6d2980b2cee7efaae8583dcb7556bba6b31604f6
Christoph Berg <myon@debian.org>
2013-02-12 08:55:13 +0100

    Remove surrogate keys from apt database schema

---
+++ pgapt-db/sql/pgdg_apt.sql
@@ -0,0 +1,108 @@
+BEGIN;
+
+CREATE SCHEMA apt;
+SET search_path TO apt;
+
+CREATE EXTENSION debversion;
+CREATE EXTENSION hstore;
+
+
+CREATE OR REPLACE FUNCTION control2hstore (control text)
+RETURNS hstore LANGUAGE sql IMMUTABLE AS
+$$SELECT regexp_replace (regexp_replace ($1, '([\\"])', '\\\1', 'g'),
+	E'^([^:]*): (.*(?:\n .*)*)', '"\1"=>"\2",', 'gn')::hstore$$;
+-- intentionally no E'' in the first line
+
+
+-- ARCHIVE-WIDE DATA
+
+CREATE TABLE architecture (
+	architecture text PRIMARY KEY
+);
+COMMENT ON TABLE architecture IS 'All known architectures, including all, excluding source';
+
+
+CREATE TABLE srcdistribution (
+	distribution text NOT NULL,
+	component text NOT NULL,
+	last_update timestamp with time zone,
+	active boolean NOT NULL DEFAULT TRUE,
+
+	PRIMARY KEY (distribution, component)
+);
+
+
+CREATE TABLE distribution (
+	distribution text NOT NULL,
+	component text NOT NULL,
+	architecture text NOT NULL
+		REFERENCES architecture (architecture),
+	last_update timestamp with time zone,
+	active boolean NOT NULL DEFAULT TRUE,
+
+	PRIMARY KEY (distribution, component, architecture),
+	FOREIGN KEY (distribution, component) REFERENCES srcdistribution (distribution, component)
+);
+
+
+-- PACKAGE DATA
+
+CREATE TABLE source (
+	source text NOT NULL,
+	srcversion debversion NOT NULL,
+	control text NOT NULL,
+	c hstore,
+
+	PRIMARY KEY (source, srcversion)
+);
+
+CREATE TABLE package (
+	package text NOT NULL,
+	version debversion NOT NULL,
+	arch text NOT NULL
+		REFERENCES architecture (architecture),
+	control text NOT NULL,
+	c hstore,
+	source text NOT NULL,
+	srcversion debversion NOT NULL,
+
+	PRIMARY KEY (package, version, arch)
+);
+ALTER TABLE package ADD FOREIGN KEY (source, srcversion) REFERENCES source (source, srcversion);
+
+
+-- SUITE DATA
+
+CREATE TABLE sourcelist (
+	distribution text NOT NULL,
+	component text NOT NULL,
+	source text NOT NULL,
+	srcversion debversion NOT NULL,
+
+	FOREIGN KEY (distribution, component) REFERENCES srcdistribution (distribution, component),
+	FOREIGN KEY (source, srcversion) REFERENCES source (source, srcversion)
+);
+CREATE INDEX ON sourcelist (distribution, component);
+CREATE INDEX ON sourcelist (source);
+
+CREATE TABLE packagelist (
+	distribution text NOT NULL,
+	component text NOT NULL,
+	architecture text NOT NULL,
+	package text NOT NULL,
+	version debversion NOT NULL,
+	arch text NOT NULL,
+	CHECK ((architecture = arch) OR (arch = 'all')),
+
+	FOREIGN KEY (distribution, component, architecture)
+		REFERENCES distribution (distribution, component, architecture),
+	FOREIGN KEY (package, version, arch) REFERENCES package (package, version, arch)
+);
+CREATE INDEX ON packagelist (distribution, component, architecture);
+CREATE INDEX ON packagelist (package);
+
+
+GRANT USAGE ON SCHEMA apt TO PUBLIC;
+GRANT SELECT ON ALL TABLES IN SCHEMA apt TO PUBLIC;
+
+COMMIT;
---
+++ pgapt-db/sql/pgdg_apt_data.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+INSERT INTO srcdistribution
+    SELECT * FROM (VALUES ('sid-pgdg'), ('wheezy-pgdg'), ('squeeze-pgdg')) dist(distribution),
+                  (VALUES ('main'), ('9.2'), ('9.1'), ('9.0'), ('8.4'), ('8.3')) comp(component)
+    WHERE NOT EXISTS (SELECT * FROM srcdistribution
+                      WHERE (distribution, component) = (dist.distribution, comp.component));
+
+INSERT INTO distribution
+    SELECT * FROM (VALUES ('sid-pgdg'), ('wheezy-pgdg'), ('squeeze-pgdg')) dist(distribution),
+                  (VALUES ('main'), ('9.2'), ('9.1'), ('9.0'), ('8.4'), ('8.3')) comp(component),
+                  (VALUES ('amd64'), ('i386')) arch(architecture)
+    WHERE NOT EXISTS (SELECT * FROM distribution
+          WHERE (distribution, component, architecture) = (dist.distribution, comp.component, arch.architecture));
+
+COMMIT;
